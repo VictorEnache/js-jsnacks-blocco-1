509052d295eebb7d7eee82c73f510ee57cf97774
VictorEnache <victor22enache@gmail.com>
2022-03-30 15:19:50 +0200

    snack_8

---
+++ SNACK_8/main.js
@@ -0,0 +1,32 @@
+/* 
+
+Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
+
+*/
+
+const numero_inserito = prompt('inserisci un numero formato da 4 cifre');
+
+
+if (numero_inserito.length === 4 && !isNaN(numero_inserito)){
+
+    let lista_numeri = Array.from(numero_inserito)
+    
+    let numero
+    
+    somma = 0;
+    
+    for (i= 0; i < lista_numeri.length; i++){
+    
+        numero = Number(lista_numeri[i])
+    
+        somma += numero
+    }
+    
+    console.log(somma)
+
+}
+else{
+    alert('devi inserire quattro numeri')
+}
+
