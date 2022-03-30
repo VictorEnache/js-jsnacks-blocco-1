67b012f0f230cbfe390c989581e47212050d989e
VictorEnache <victor22enache@gmail.com>
2022-03-30 11:58:02 +0200

    esercizio 3

---
+++ SNACK_3/main.js
@@ -0,0 +1,27 @@
+/* 
+
+SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+*/
+let numero
+
+let lista_numeri = []
+
+let somma = 0;
+
+for (i = 0; i < 10; i++){
+    numero = parseInt(prompt('Inserisci un numero'))
+    
+    lista_numeri.push(numero)
+    
+   
+}
+
+for (i = 0; i < lista_numeri.length; i++){
+    somma += lista_numeri[1]
+}
+
+console.log(somma)
+
