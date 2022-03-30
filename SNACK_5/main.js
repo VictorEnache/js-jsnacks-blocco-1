ce13bf6a50843e2abc0193ae0a57eb1efa82422c
VictorEnache <victor22enache@gmail.com>
2022-03-30 12:38:26 +0200

    es 5

---
+++ SNACK_5/main.js
@@ -0,0 +1,21 @@
+/* 
+
+SNACK 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+*/
+
+let elenco_numeri = []
+
+let numero_inserito
+
+for (i = 0; i < 6; i++) {
+    numero_inserito = parseInt(prompt('inserisci un numero'))
+
+    if (numero_inserito % 2 != 0){
+        elenco_numeri.push(numero_inserito)
+    }
+}
+
+console.log(elenco_numeri)
